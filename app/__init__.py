aedf2ba7d3075836953c173791d4b0ac363301b9
Zaki Ali <zakiali88@gmail.com>
2016-11-11 14:40:51 -0800

    update __init__.py to includ Flask-Login.
    Starting work on login part of app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+import os 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
 
-from app import views
+from app import views, models
