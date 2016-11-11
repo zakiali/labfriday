21d044bd47220c5ca4dbdb2768f9be4d6052727f
Zaki Ali <zakiali88@gmail.com>
2016-11-11 14:40:08 -0800

    create models.py which holds database schema

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    lunches = db.relationship('Lunches', backref='author', lazy='dynamic')
+    
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+class Lunches(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant = db.Column(db.String(50))
+    datestamp = (db.Column(db.Date))
+    order = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return '<Restaurant %r>' % (self.restaurant)
