7ac185b2dd903cb485903b1a855debcf9c7748df
Zaki Ali <zakiali88@gmail.com>
2016-11-11 15:31:21 -0800

    update forms and views

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,10 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField, DateField
 from wtforms.validators import DataRequired
 
-class LoginForm(Form):
+class SignupForm(Form):
     name = StringField('name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     date = DateField('date', format='%m/%d/%Y', validators=[DataRequired()]) 
+
+class LoginForm():
+    name = StringField('name', calidators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,17 +1,29 @@
-from flask import render_template, flash, redirect, request, url_for
-from app import app
-from .forms import LoginForm
+from flask import render_template, flash, redirect, request, url_for, g
+from flask_login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
+from .forms import LoginForm, SignupForm
+from .models import User
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/', methods=['GET','POST'])
-def login():
-    error = None
-    form=LoginForm()
+def signup():
+    form=SignupForm()
     if form.validate_on_submit():
         flash('Login requested for name="%s", email=%s, data=%s'%(form.name.data, form.email.data, str(form.date.data)))
         return redirect('success')
     return render_template('login.html',
                             title='Sign Up', 
                             form=form)
+
+@app.route('/orderer', methods=['GET','POST'])
+def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('success'))
+    form = LoginForm()
+
 
 @app.route('/success', methods=['GET'])
 def success():
