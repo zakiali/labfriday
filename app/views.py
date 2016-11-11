b8b99960d0fc1e24d1b67f22f3985cb740d22ccc
Zaki Ali <zakiali88@gmail.com>
2016-11-11 14:37:55 -0800

    update routes for views.py

--- app/views.py
+++ app/views.py
@@ -2,21 +2,14 @@ from flask import render_template, flash, redirect, request, url_for
 from app import app
 from .forms import LoginForm
 
-@app.route('/', methods=['GET'])
-#@app.route('/index', methods=['GET'])
-def index():
-    return redirect('/login')
-
-@app.route('/login', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def login():
-    next = request.form.get('next')
-    if next is None:
-        next=url_for('index')
+    error = None
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for name="%s", data=%s'%(form.name.data, str(form.date.data)))
+        flash('Login requested for name="%s", email=%s, data=%s'%(form.name.data, form.email.data, str(form.date.data)))
         return redirect('success')
-    return render_template('index.html',
+    return render_template('login.html',
                             title='Sign Up', 
                             form=form)
 
