4f7ec981213ccb0c7e34b8b4b51ecfd126394040
Zaki Ali <zakiali88@gmail.com>
2016-11-13 21:16:59 -0800

    removed stuff for logging in models.py
    Decided not to have users log in per-se

--- app/models.py
+++ app/models.py
@@ -6,21 +6,6 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     lunches = db.relationship('Lunches', backref='author', lazy='dynamic')
     
-    @property
-    def is_authenticated(self):
-        return True
-    
-    @property
-    def is_active(self):
-        return True
-    
-    @property 
-    def is_anononymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.id)
-    
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
