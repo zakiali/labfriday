7eb920df5f25499fc4211aa098d438a246adabe2
Zaki Ali <zakiali88@gmail.com>
2016-11-13 21:16:38 -0800

    updated views to add user's and lunches to database.

--- app/views.py
+++ app/views.py
@@ -1,31 +1,51 @@
 from flask import render_template, flash, redirect, request, url_for, g
-from flask_login import login_user, logout_user, current_user, login_required
-from app import app, db, lm
+from app import app, db
 from .forms import LoginForm, SignupForm
-from .models import User
+from .models import User, Lunches
 
-@lm.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def gen_last_n_orders(n=4):
+    recents = []
+    for lunch in Lunches.query.order_by('datestamp desc').all()[:n]:
+        recents.append(dict(zip(('date', 'name', 'restaurant'),(lunch.datestamp, lunch.author.nickname, lunch.restaurant))))
+    return recents
+
+def add_user_to_db(name, email, date):
+    #check to see if in data base already by using email.
+    user = User.query.filter_by(email=email).first()
+    if user == None:
+        #if not in db, add user and lunch session
+        user = User(nickname=name, email=email)
+        db.session.add(user)
+        l = Lunches(datestamp=date, author=user)
+        db.session.add(l)
+    else: 
+        #if user in db, add lunch with author as user
+        l = Lunches(datestamp=date, author=user)
+        db.session.add(l)
+    db.session.commit()
+    return None
+    
 
 @app.route('/', methods=['GET','POST'])
 def signup():
+    #get persons name who is ordering lab friday lunch this week
     form=SignupForm()
     if form.validate_on_submit():
-        flash('Login requested for name="%s", email=%s, data=%s'%(form.name.data, form.email.data, str(form.date.data)))
+        name, email, date = form.name.data, form.email.data, form.date.data
+        flash('Login requested for name="%s", email=%s, date=%s'%(name, email, date))
+        add_user_to_db(name, email, date)
         return redirect('success')
     return render_template('login.html',
                             title='Sign Up', 
-                            form=form)
-
-@app.route('/orderer', methods=['GET','POST'])
-def login():
-    if g.user is not None and g.user.is_authenticated:
-        return redirect(url_for('success'))
-    form = LoginForm()
-
+                            form=form,
+                            recents=gen_last_n_orders())
 
 @app.route('/success', methods=['GET'])
 def success():
-    return render_template('success.html', title='Thank You')
+    form = LoginForm()
+    return render_template('success.html', 
+                          title='Thank You',
+                          recents=gen_last_n_orders())
 
+
+#@app.route('/user/<nickname>')
