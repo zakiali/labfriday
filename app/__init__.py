68c65e8e7a79d2f272e16e202baca18e4fff1f29
Zaki Ali <zakiali88@gmail.com>
2016-11-27 10:30:07 -0800

    import FlaskMail into __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
+mail = Mail(app)
+
 
 from app import views, models
