2af19fccda593cf86291f1743918275e24a770c3
Zaki Ali <zakiali88@gmail.com>
2016-11-27 10:30:51 -0800

    Add in email variables into config file

--- config.py
+++ config.py
@@ -8,4 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = 'admin@labfriday.com'
+
 
