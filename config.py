8fae96209191e52ebbec1ec1f3c35dce1b0def21
Zaki Ali <zakiali88@gmail.com>
2016-11-28 21:02:17 -0800

    update some configuration variables.
    SERVER_NAME : To get "get_url" to work in the reminder and followup html templates.
    SQLALCHEMY_TRACK_MODIFICATIONS : To stop warning from printing when starting up app.
    Add in Mail Server config variables

--- config.py
+++ config.py
@@ -5,14 +5,17 @@ SECRET_KEY = 'you-will-never-guess'
 import os 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SERVER_NAME='127.0.0.1:5000'
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
+#MAIL_USE_SSL = True
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = 'admin@labfriday.com'
 
 
