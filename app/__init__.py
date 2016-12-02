372d7073cab7f6118015874f4d1919d732c67aa7
Zaki Ali <zakiali88@gmail.com>
2016-12-01 22:44:10 -0800

    import celery and set it up

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_mail import Mail
+from celery import Celery
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-lm = LoginManager()
-lm.init_app(app)
 mail = Mail(app)
-
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 from app import views, models
