9dfaf7492eb9fbecd1cf9feb357bfeacc6d0af9f
Zaki Ali <zakiali88@gmail.com>
2016-12-01 22:43:52 -0800

    add celery.task for jobs

--- app/jobs.py
+++ app/jobs.py
@@ -1,12 +1,14 @@
 from views import get_next_volunteer, get_todays_volunteer
 import emails
 import time
+from app import celery
 
+@celery.task
 def reminder_email():
     lunch = get_next_volunteer()
     if lunch != None:
         emails.reminder_email(lunch) 
-    
+@celery.task
 def followup_email():
     lunch = get_todays_volunteer()
     if lunch != None:
