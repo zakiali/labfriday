5843b97fb3fed7d73bb5da53b5fcf881a2a3b2a7
Zaki Ali <zakiali88@gmail.com>
2016-12-03 17:43:46 -0800

    update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,17 +1,18 @@
-from views import get_next_volunteer, get_todays_volunteer
+import views #app.views import get_next_volunteer, get_todays_volunteer
 import emails
 import time
 from app import celery
 
 @celery.task
 def reminder_email():
-    lunch = get_next_volunteer()
+    lunch = views.get_next_volunteer()
     if lunch != None:
         emails.reminder_email(lunch) 
+    print 'sending reminder email to {0}'.format(lunch.author.nickname)
+
 @celery.task
 def followup_email():
-    lunch = get_todays_volunteer()
+    lunch = views.get_todays_volunteer()
     if lunch != None:
         emails.followup_email(lunch)
-       
-
+    print 'sending followup email to {0}'.format(lunch.author.nickname)
