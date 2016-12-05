c70b83041bad974d702e2fedc6a13fc1d07d9fc5
Zaki Ali <zakiali88@gmail.com>
2016-12-04 19:51:04 -0800

    update run.py

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
 #! /usr/bin/env python
 from app import app
 import time
-from app.scheduler import Scheduler
-from app import jobs
 
 if __name__ == '__main__':
-    scheduler = Scheduler()
-    scheduler.every().day.at("20:44").do(jobs.reminder_email)
-    scheduler.every().day.at("20:44").do(jobs.followup_email)
-    scheduler.run_continuously()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)#, use_reloader=False)
 
