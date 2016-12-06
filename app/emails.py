a485b908e48ec03e6619e1e7f9ef102369fd075e
Zaki Ali <zakiali88@gmail.com>
2016-12-05 16:40:05 -0800

    update emails.py with new food is here email.
    update so that it uses default sender.

--- app/emails.py
+++ app/emails.py
@@ -3,9 +3,9 @@ from flask_mail import Message
 from app import app,mail
 import datetime
 
-def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(subject, recipients, text_body, html_body, **kwargs):
     '''Generic function to send an email'''
-    msg = Message(subject, sender=sender, recipients=recipients)
+    msg = Message(subject, recipients=recipients, **kwargs)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
@@ -14,20 +14,27 @@ def send_email(subject, sender, recipients, text_body, html_body):
 def reminder_email(lunch_model): 
     '''Send a reminder email'''
     subject='[labfriday] You are ordering lab lunch this week! Thanks you!'
-    sender = 'zakiali88@gmail.com'
     recipients = [lunch_model.author.email]
     with app.app_context():
         text_body = render_template("reminder.txt", lunch=lunch_model)
         html_body = render_template("reminder.html", lunch=lunch_model)
-        send_email(subject, sender, recipients, text_body, html_body)
+        send_email(subject, recipients, text_body, html_body)
 
 def followup_email(lunch_model):
     '''Send a follow up email'''
     subject='[labfriday] Thank you for feeding us!'
-    sender = 'zakiali88@gmail.com'
     recipients = [lunch_model.author.email]
     with app.app_context():
         text_body = render_template("followup.txt", lunch=lunch_model)
         html_body = render_template("followup.html", lunch=lunch_model)
-        send_email(subject, sender, recipients, text_body, html_body)
+        send_email(subject, recipients, text_body, html_body)
    
+
+def foodishere_email(lunch_model):
+    '''Send an email to labfriday list that food for today is here'''
+    subject='[labfriday] Food is here!'
+    recipients = ['zakiali88@gmail.com'] #['labfriday@lists.berkeley.edu']
+    with app.app_context():
+        text_body = render_template("foodshere.txt", lunch=lunch_model)
+        html_body = render_template("foodshere.html", lunch=lunch_model)
+        send_email(subject, recipients, text_body, html_body)
